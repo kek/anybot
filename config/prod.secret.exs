9fe5e1ccae7ae21bcf96f9c5b9da13b321267157
Karl Eklund <localpart@gmail.com>
2019-11-20 00:39:39 +0100

    Run web server in release

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -19,8 +19,8 @@ config :anybot, AnybotWeb.Endpoint,
 #
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
-#
-#     config :anybot, AnybotWeb.Endpoint, server: true
-#
+
+config :anybot, AnybotWeb.Endpoint, server: true
+
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
