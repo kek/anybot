7133191e0c1c2ab80a274f1f4996b73c0ffa03ac
Karl Eklund <localpart@gmail.com>
2019-11-21 18:43:05 +0100

    fix log level in prod?

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,8 @@ config :anybot, AnybotWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-# Do not print debug messages in production
-config :logger, level: :info
+# # Do not print debug messages in production
+# config :logger, level: :info
 
 # ## SSL Support
 #
