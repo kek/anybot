b9d3d3eaaddb142deecab8a41ec8d569ad833777
Karl Eklund <localpart@gmail.com>
2019-11-21 19:27:52 +0100

    How do I get the request body?

--- lib/anybot_web/controllers/event_controller.ex
+++ lib/anybot_web/controllers/event_controller.ex
@@ -2,11 +2,22 @@ defmodule AnybotWeb.EventController do
   use AnybotWeb, :controller
   require Logger
 
-  def create(conn, params) do
+  def create(
+        conn,
+        %{"challenge" => challenge, "token" => token, "type" => "url_verification"} = params
+      ) do
+    [slack_request_timestamp] = get_req_header(conn, "x-slack-request-timestamp")
+    [slack_signature] = get_req_header(conn, "x-slack-signature")
+    _ = challenge
+    _ = token
+    _ = slack_request_timestamp
+    _ = slack_signature
     Logger.info(inspect(conn.req_headers))
     Logger.info(inspect(params))
 
+    response = "ok"
+
     conn
-    |> send_resp(200, "ok")
+    |> send_resp(200, response)
   end
 end
