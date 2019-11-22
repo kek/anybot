1a031a820c9abb9bb98fd3d756f6f8476f7ae880
Karl Eklund <localpart@gmail.com>
2019-11-22 11:41:45 +0100

    verify unhandled events too

--- config/config.exs
+++ config/config.exs
@@ -23,7 +23,8 @@ config :anybot,
   slack_app_id: System.get_env("SLACK_APP_ID"),
   slack_client_id: System.get_env("SLACK_CLIENT_ID"),
   slack_client_secret: System.get_env("SLACK_CLIENT_SECRET"),
-  slack_signing_secret: System.get_env("SLACK_SIGNING_SECRET")
+  slack_signing_secret: System.get_env("SLACK_SIGNING_SECRET"),
+  slack_bot_token: System.get_env("SLACK_BOT_TOKEN")
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
--- lib/anybot_web/controllers/event_controller.ex
+++ lib/anybot_web/controllers/event_controller.ex
@@ -4,37 +4,22 @@ defmodule AnybotWeb.EventController do
 
   def create(
         conn,
-        %{"challenge" => challenge, "token" => token, "type" => "url_verification"} = params
+        %{"challenge" => challenge, "type" => "url_verification"} = params
       ) do
-    [slack_request_timestamp] = get_req_header(conn, "x-slack-request-timestamp")
-    [slack_signature] = get_req_header(conn, "x-slack-signature")
-    _ = challenge
-    _ = token
-    _ = slack_request_timestamp
-    _ = slack_signature
     Logger.debug(inspect(conn.req_headers))
     Logger.debug(inspect(params))
+    [slack_request_timestamp] = get_req_header(conn, "x-slack-request-timestamp")
+    [slack_signature] = get_req_header(conn, "x-slack-signature")
 
-    body = conn.assigns.raw_body
-
-    basestring = "v0:#{slack_request_timestamp}:#{body}"
-
-    signed =
-      "v0=" <>
-        (:crypto.hmac(:sha256, Application.get_env(:anybot, :slack_signing_secret), basestring)
-         |> Base.encode16()
-         |> String.downcase())
-
-    if signed == slack_signature do
-      Logger.info("Computed signature #{signed} did match incoming signature #{slack_signature}")
-
+    if verify(
+         conn.assigns.raw_body,
+         slack_request_timestamp,
+         slack_signing_secret(),
+         slack_signature
+       ) do
       conn
       |> send_resp(200, challenge)
     else
-      Logger.info(
-        "Computed signature #{signed} did not match incoming signature #{slack_signature}"
-      )
-
       conn
       |> send_resp(403, "no")
     end
@@ -42,8 +27,46 @@ defmodule AnybotWeb.EventController do
 
   def create(conn, _) do
     Logger.info("Unhandled event: " <> inspect(conn.assigns.raw_body))
+    [slack_signature] = get_req_header(conn, "x-slack-signature")
+    [slack_request_timestamp] = get_req_header(conn, "x-slack-request-timestamp")
+
+    if verify(
+         conn.assigns.raw_body,
+         slack_request_timestamp,
+         slack_signing_secret(),
+         slack_signature
+       ) do
+      Logger.info("Verified signature")
+    else
+      Logger.info("Bad signature")
+    end
 
     conn
     |> send_resp(200, "ok")
+  end
+
+  defp verify(body, slack_request_timestamp, slack_signing_secret, slack_signature) do
+    basestring = "v0:#{slack_request_timestamp}:#{body}"
+
+    signed =
+      "v0=" <>
+        (:crypto.hmac(:sha256, slack_signing_secret, basestring)
+         |> Base.encode16()
+         |> String.downcase())
+
+    if slack_signature == signed do
+      Logger.info("Computed signature #{signed} did match incoming signature #{slack_signature}")
+      true
+    else
+      Logger.info(
+        "Computed signature #{signed} did not match incoming signature #{slack_signature}"
+      )
+
+      false
+    end
+  end
+
+  defp slack_signing_secret do
+    Application.get_env(:anybot, :slack_signing_secret)
   end
 end
