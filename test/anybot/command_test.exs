d46d89b768b53738faab6076b2e058d3351315b6
Karl Eklund <localpart@gmail.com>
2019-11-30 12:28:38 +0100

    Clean up

--- test/anybot/command_test.exs
+++ test/anybot/command_test.exs
@@ -17,19 +17,4 @@ defmodule Anybot.CommandTest do
   test "Don't know" do
     assert Anybot.Command.parse("!something") == {:error, "Unknown command !something"}
   end
-
-  # ### `!save <name> <program>`
-  # Save a program and run it. The code will be run again at restart.
-
-  # ### `!delete <name>`
-  # Removes a program so that it won't be run at next restart.
-
-  # ### `!list`
-  # List saved programs.
-
-  # ### `!show <name>`
-  # Shows the code for a saved program.
-
-  # ### `!help`
-  # Display help for these commands.
 end
