e5ceb8c962b95cf88acdf5ed881581921e88230b
Karl Eklund <localpart@gmail.com>
2019-12-01 13:28:07 +0100

    Make hostname configurable

--- config/releases.exs
+++ config/releases.exs
@@ -7,3 +7,5 @@ config :anybot,
   slack_signing_secret: System.get_env("SLACK_SIGNING_SECRET"),
   slack_bot_token: System.get_env("SLACK_BOT_TOKEN"),
   storage_directory: Path.join(System.get_env("HOME"), ".anybot-storage")
+
+config :anybot, AnybotWeb.Endpoint, url: [host: System.get_env("WEB_SERVER_HOST"), port: 443]
